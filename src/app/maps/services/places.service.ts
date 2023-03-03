4e44f43cd1cb993b25503a1eb3e8c9eb9952e4f7
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-03-02 23:50:20 -0300

    interface and http requerst

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +8,9 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public userLocation?: [number, number];
+
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.userLocation;
@@ -28,7 +33,20 @@ export class PlacesService {
     })
   }
 
-  constructor() {
+  getPlacesByQuery(query: string = '') {
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=5&proximity=-64.19084689256306%2C-31.39028741611226&language=es&access_token=pk.eyJ1IjoiZXJpY2RlbmlzLTE5ODgiLCJhIjoiY2xla2VpYTV5MGpqbDN3bzZoYTJjaTJjNiJ9.8MKm8rvuDojCSKU1y9RPHQ`)
+      .subscribe(resp => {
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      })
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {
     this.getUserLocation();
   }
 }
